2a0bc76d48b128377cf63194ea23fc108e853401
Sujit27 <sujit271290@gmail.com>
2020-03-23 16:01:09 +0100

    Created Dataset class for Phoc Dataset

---
+++ library/phoc_net/phoc_dataset.py
@@ -0,0 +1,63 @@
+import os
+
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import torch
+from torch.utils.data import Dataset
+
+
+import dagtasets as dg
+#from cnn_ws.io.list_io import LineListIO
+from phoc_embedding import build_phoc_descriptor, get_unigrams_from_strings
+#from cnn_ws.transformations.image_size import check_size
+#from cnn_ws.transformations.homography_augmentation import HomographyAugmentation
+
+
+class PhocDataset(Dataset):
+    '''
+    Phoc dataset class for the mjsynth dataset
+    '''
+
+    def __init__(self, root_dir,phoc_unigram_levels=(1, 2, 4, 8)):
+
+        # class members
+        self.word_list = None
+        self.word_string_embeddings = None
+        self.query_list = None
+        self.label_encoder = None
+
+        transform = dg.mjsynth.mjsynth_gray_scale
+        self.ds = dg.mjsynth.MjSynthWS(root_dir,transform)
+        
+        annotation_file = os.path.join(root_dir,'raw',"annotation_train.txt")
+        with open(annotation_file) as f:
+            lines = [line.rstrip() for line in f]
+        words = [((line.split("_"))[1]).lower() for line in lines]
+
+        # compute a mapping from class string to class id
+        self.label_encoder = LabelEncoder()
+        self.label_encoder.fit(words)
+
+        # extract unigrams from train split
+        unigrams = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+        
+        word_embeddings = build_phoc_descriptor(words=words,phoc_unigrams=unigrams,unigram_levels=phoc_unigram_levels)
+        
+        self.word_list = words
+        self.word_string_embeddings = word_embeddings.astype(np.float32)
+
+    def embedding_size(self):
+        return len(self.word_string_embeddings[0])
+
+
+    def __len__(self):
+        return len(self.word_list)
+
+    def __getitem__(self, index):
+        word_img, _ = self.ds[index]
+        embedding = self.word_string_embeddings[index]
+        embedding = torch.from_numpy(embedding)
+        class_id = self.label_encoder.transform([self.word_list[index]])
+
+        return word_img, embedding, class_id
+
