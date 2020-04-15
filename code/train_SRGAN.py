eb6871d4fa17610db9b06352753f2cb3e3f2a900
Sujit27 <sujit271290@gmail.com>
2020-04-15 16:14:54 +0200

    SRGAN dataset loader getter modified to also return label of the dream. Extra loss added to the training of generator, this loss is obtained by forward passing the dreams generated through the trained classifier

--- code/train_SRGAN.py
+++ code/train_SRGAN.py
@@ -2,6 +2,7 @@
 
 import sys
 sys.path.append("../library/SR_GAN")
+sys.path.append("../library")
 import argparse
 import os
 from pathlib import Path
@@ -21,10 +22,12 @@ import torchvision.transforms as transforms
 
 import dagtasets as dg
 from real_dream_dataset import *
+from dict_net import *
 from models import Generator, Discriminator
 #from utils import Visualizer
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--network_model', type=str, default='../models2/net_1000_0.001_200_0.0.pth', help='Trained classifier network')
 parser.add_argument('--model_save_loc', type=str, default='.', help='location where the generator checkpoints are saved')
 parser.add_argument('--real_dataroot', type=str, default='/var/tmp/on63ilaw/mjsynth', help='path to real images dataset')
 parser.add_argument('--dream_dataroot', type=str, default='/var/tmp/on63ilaw/mjsynth/sample_dreams_dataset', help='path to dream images dataset')
@@ -34,6 +37,7 @@ parser.add_argument('--nEpochs', type=int, default=20, help='number of epochs to
 parser.add_argument('--disp', type=int, default=50, help='number of iterations for display of losses')
 parser.add_argument('--generatorLR', type=float, default=0.00001, help='learning rate for generator')
 parser.add_argument('--discriminatorLR', type=float, default=0.00001, help='learning rate for discriminator')
+parser.add_argument('--gen_loss_ratio', type=float, default=0.7, help='ratio of two different types of losses for the generator, should be less than one ')
 parser.add_argument('--nGPU', type=int, default=1, help='number of GPUs to use')
 
 opt = parser.parse_args()
@@ -43,9 +47,22 @@ Path("./out").mkdir(parents=True,exist_ok=True)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+def find_dream_acc_loss(network,input_images,target_labels,loss_criterion):
+    output = network(input_images)
+    loss = loss_criterion(output,target_labels)
+
+    return loss
+
 def main():
 
     dataset = RealDreamDataset(opt.real_dataroot,opt.dream_dataroot)
+
+    num_labels = int(((os.path.basename(opt.network_model)).split("_"))[1])
+    network = DictNet(num_labels)
+    network.load_state_dict(torch.load(opt.network_model))
+    network.eval()
+    network.to(device)
+    loss_criterion = nn.CrossEntropyLoss()
 
     # create dataloaders
     data_loader = torch.utils.data.DataLoader(dataset,batch_size=opt.batchSize,
@@ -74,22 +91,20 @@ def main():
     optim_generator = optim.Adam(generator.parameters(), lr=opt.generatorLR)
     optim_discriminator = optim.Adam(discriminator.parameters(), lr=opt.discriminatorLR)
 
-    ############################### PRETRAIN GENERATOR !!! #####################################
-
-
 
     ####################################
     # SRGAN training
-    print("Generator loss, Discriminator loss")
+    print("Generator adv loss, Generator dream acc loss, Discriminator adv loss")
     for epoch in range(opt.nEpochs):
 
         for i, data in enumerate(data_loader):
 
             # extract images
-            real_images,dream_images= data
+            real_images,dream_images,dream_labels= data
 
             real_images = real_images.to(device)
             dream_images = dream_images.to(device)
+            dream_labels = dream_labels.to(device)
 
             # pass the dream images through generator
             dream_images_generated = generator(dream_images)
@@ -101,21 +116,23 @@ def main():
             real_output = discriminator(real_images)
             dream_output = discriminator(dream_images_generated)
 
-            discriminator_loss = adversarial_criterion(real_output, ones_const) + adversarial_criterion(dream_output, zeros_const)
+            discriminator_loss = (adversarial_criterion(real_output, ones_const) + adversarial_criterion(dream_output, zeros_const))/opt.batchSize
             
             discriminator_loss.backward(retain_graph=True)
             optim_discriminator.step()
 
             ######### Train generator #########
+            generator_dream_loss = find_dream_acc_loss(network,dream_images_generated,dream_labels,loss_criterion)/opt.batchSize
             generator.zero_grad()
+            generator_adv_loss = (adversarial_criterion(dream_output, ones_const))/opt.batchSize
 
-            generator_loss = adversarial_criterion(dream_output, ones_const)
+            generator_loss = opt.gen_loss_ratio * generator_dream_loss + (1-opt.gen_loss_ratio) * generator_adv_loss
             
             generator_loss.backward()
             optim_generator.step()  
 
             if i % opt.disp == 0:
-                print("{},{}".format(generator_loss.item(),discriminator_loss.item())) 
+                print("{},{},{}".format(generator_adv_loss.item(),generator_dream_loss.item(),discriminator_loss.item())) 
         # Do checkpointing
         if epoch % 5 == 4:
             generator_checkpoint = 'generator_checkpoint_' + str(epoch) + '.pth'
--- library/SR_GAN/real_dream_dataset.py
+++ library/SR_GAN/real_dream_dataset.py
@@ -1,4 +1,6 @@
+import os
 import random
+from sklearn.preprocessing import LabelEncoder
 import torch
 import dagtasets as dg
 from torch.utils.data import Dataset
@@ -15,6 +17,14 @@ class RealDreamDataset(Dataset):
         self.transform = transform
 
         self.real_dataset = self.truncate_real_dataset(self.real_dataset,len(self.dream_dataset))
+        
+        annotation_file = os.path.join(dream_dataroot,'raw',"annotation_train.txt")
+        with open(annotation_file) as f:
+            lines = [line.rstrip() for line in f]
+        words = [((line.split("_"))[1]).lower() for line in lines]
+        self.label_encoder = LabelEncoder()
+        self.label_encoder.fit(words)
+
 
     def truncate_real_dataset(self,dataset,reduced_size):
         indices_list = [i for i in range(len(dataset))]
@@ -30,5 +40,8 @@ class RealDreamDataset(Dataset):
         real_image,_ = self.real_dataset[index]
         dream_image,_ = self.dream_dataset[index]
 
-        return real_image,dream_image
+        word = self.dream_dataset.filenames[index].split("_")[1]
+        target_label = self.label_encoder.transform([word])
+
+        return real_image,dream_image,target_label[0]
 
